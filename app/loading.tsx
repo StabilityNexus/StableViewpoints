b4d21e6d440a8b87aeb14d3fea2b6fb0f9dc5249
Atharva Naik <atharvan.coder@gmail.com>
2026-04-11 16:49:43 +0530

    Refactor: use next/image for loading asset to optimize performance

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,15 @@
+import Image from "next/image";
+
 export default function Loading() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-green-50 via-yellow-50 to-[#FFC517]/10 flex items-center justify-center">
       <div className="text-center">
-        <img
+        <Image
           src="/logo-animated.gif"
           alt="Loading..."
+          width={80}
+          height={80}
+          unoptimized
           className="w-20 h-20 mx-auto mb-4"
         />
         <p className="text-gray-600">Loading articles...</p>
